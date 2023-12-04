7a4f7e79b68877d2a7970a78286051cd27cfe091
Olya <lolcap202@gmail.com>
2023-12-04 23:56:12 +0600

    add zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем произвольную строку
+        string inputString = "Happy New Year";
+
+        // Проверяем, является ли строка палиндромом
+        bool isPalindrome = IsPalindrome(inputString);
+
+        // Выводим результат
+        Console.WriteLine("Строка: " + inputString);
+        Console.WriteLine("Является ли палиндромом: " + isPalindrome);
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        // Удаляем из строки все пробелы и приводим к нижнему регистру
+        string cleanStr = str.Replace(" ", "").ToLower();
+
+        // Создаем обратную копию строки
+        char[] charArray = cleanStr.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedStr = new string(charArray);
+
+        // Сравниваем исходную строку с ее обратной копией
+        return cleanStr == reversedStr;
+    }
+}
+
