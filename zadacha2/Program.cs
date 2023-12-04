c23e92e253d09fa199d7a8b0bd51831097496e83
Olya <lolcap202@gmail.com>
2023-12-04 23:52:34 +0600

    add zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем строку с латинскими буквами 
+        string originalString = "Happy New Year My Friend!";
+
+        // Преобразуем все заглавные буквы в строчные
+        string resultString = ConvertToLowerCase(originalString);
+
+        // Выводим результат
+        Console.WriteLine("Исходная строка: " + originalString);
+        Console.WriteLine("Результат: " + resultString);
+    }
+
+    static string ConvertToLowerCase(string inputString)
+    {
+        // Используем метод ToLower для преобразования всех заглавных букв в строчные
+        return inputString.ToLower();
+    }
+}
